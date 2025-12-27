d1c44220af06887adaa0794bff636c81af43ee1e
Chris Olsen <chrisolsenweb@gmail.com>
2025-12-26 21:35:53 -0500

    reset

--- backend/app/api/v1/routers.py
+++ backend/app/api/v1/routers.py
@@ -5,3 +5,6 @@ from app.api.v1.endpoints import hello
 api_router = APIRouter()
 api_router.include_router(hello.router)
 api_router.include_router(users.router)
+
+api_router.include_router(todo_items.router, prefix='/todo_items', tags=['TodoItem'])
+api_router.include_router(todo_items.router, prefix='/todo_items', tags=['TodoItem'])
